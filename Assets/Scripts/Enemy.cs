73d8915299e8d9f0a31d70ded0bd0f4fc4785d03
Maxim Podgorov <mapodgorov@gmail.com>
2020-06-29 03:40:31 +0700

    Add enemy script

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int _health = 10;
+    [SerializeField] private int _reward = 10;
+
+    public void TakeDamage(int damage)
+    {
+        _health -= damage;
+        if (_health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
