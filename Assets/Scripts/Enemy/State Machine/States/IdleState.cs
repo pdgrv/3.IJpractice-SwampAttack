4e21af20970aa2fdf750169a3a6836f4b2543d02
Maxim Podgorov <mapodgorov@gmail.com>
2020-07-01 01:31:00 +0700

    Add goblin assets, IdleState, null-condition at DistanceTransition

---
+++ Assets/Scripts/Enemy/State Machine/States/IdleState.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class IdleState : State
+{
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    private void OnEnable()
+    {
+        _animator.Play("Idle");
+    }
+
+    private void OnDisable()
+    {
+        _animator.StopPlayback();
+    }
+}
